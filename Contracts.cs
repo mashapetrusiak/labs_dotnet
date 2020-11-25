10cd15d3c3cc7412d642f51c7d50008b5c1eca3f
Masha_Pertusiak <57170071+mashapetrusiak@users.noreply.github.com>
2020-11-25 20:15:48 +0200

    add lab 1

---
+++ Contracts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLab
+{
+    class Contracts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SurName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int Home { get; set; }
+        public string PositionName { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime ProjectStart { get; set; }
+        public DateTime ProjectEnd { get; set; }
+        public string ProjectDescription { get; set; }
+        public string Characterization { get; set; }
+        public int Days { get; set; }
+        public int Salary { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + SurName + " " + City + " " + Street + " " + Home + " " + PositionName + " " + ProjectStart + " " +
+                ProjectEnd + " " + ProjectDescription + " " + Characterization + " " + Days + " " + Salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace FirstLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnection connection = new DBConnection();
+            List<Contracts> contracts = connection.GetContracts();
+            contracts.ForEach(contract => Console.WriteLine(contract));
+            Console.ReadKey();
+        }
+    }
+}
